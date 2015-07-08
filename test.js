01e316a00ed7532e4f3bf2f13ad145ba51f1f32c
root <maxime_labelle@hotmail.com>
2015-07-08 16:37:55 -0400

    .

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
+var start = new Date().getTime();
 
 var Memento = require('./index');
 
 var mysqlConfig = {
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'benchmark'
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'benchmark'
 };
 
 var memcachedConfig = "127.0.0.1:11211";
@@ -13,7 +14,12 @@ var memcachedConfig = "127.0.0.1:11211";
 var memento = new Memento({mysql: mysqlConfig, memcached: memcachedConfig});
 
 memento.query('SELECT * FROM test LIMIT 1', function(err, rows, fields) {
-  if (err) throw err;
+	if (err) throw err;
 
-  console.log(rows);
+	console.log(rows);
+	var end = new Date().getTime();
+	var time = end - start;
+	console.log('Execution time: ' + time);
 });
+
+
