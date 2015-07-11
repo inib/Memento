25950812d7b9d3218d41a25cff2d9d3331ebf49b
Maxime Labelle <maxime.labelle@owasp.org>
2015-07-11 10:45:18 -0400

    .

--- benchmarks/test.js
+++ benchmarks/test.js
@@ -1,6 +1,4 @@
-var start = new Date().getTime();
-
-var Memento = require('./index');
+var Memento = require('../index');
 
 var mysqlConfig = {
 	host     : 'localhost',
@@ -13,15 +11,17 @@ var memcachedConfig = "127.0.0.1:11211";
 
 var memento = new Memento({mysql: mysqlConfig, memcached: memcachedConfig});
 
-memento.query('SELECT * FROM test LIMIT 1', function(err, rows, fields) {
+var start = new Date().getTime();
+
+memento.query('SELECT * FROM test WHERE id>10000 LIMIT 5000', function(err, rows, fields) {
 	if (err) throw err;
 
-	console.log(rows);
+	//console.log(rows);
 	var end = new Date().getTime();
 	var time = end - start;
-	console.log('Execution time: ' + time);
+		
+	console.log('Execution time: ' + (time / 1000) + ' seconds');	
 	
 	memento.finish();
 });
-
 
